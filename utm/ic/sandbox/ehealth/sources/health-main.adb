7041cf23c7a95286af9dc672426ed60bfb9abee1
Ingrid CHERRY - AdaLabs Ltd <ingrid.cherry@adalabs.education>
2017-08-29 21:30:30 +0400

    Dimentional system (tbc)

--- utm/ic/sandbox/ehealth/sources/health-main.adb
+++ utm/ic/sandbox/ehealth/sources/health-main.adb
@@ -5,39 +5,49 @@ with Ada.Text_IO,
 use Ada.Text_IO;
 
 with Health.Options,
-  Health.Services;
+     Health.Services;
 
-use Health.Options;
+ use Health.Options;
 
 procedure Health.Main
 is
-   Calorie_Burned : Calorie_Type;
-   Body_Index     :BMI_Type;
-
+ Calorie_Burned : Calorie_Type;
+ Body_Index     : BMI_Type ;
+ Daily_Calorie  : Calorie_Type;
+ Ideal_Weight : Mass_Type;
 begin
-   pragma Compile_Time_Warning (True, "TECH style clean");
-   pragma Compile_Time_Warning (True, "TECH restore option");
-   pragma Compile_Time_Warning (True, "FEATURE simulated humans with different activity profiles");
-   --  calorie in per day
-   --  hour of sport () per week
 
-   Options.Initialize;
+  Options.Initialize;
 
-   Calorie_Burned := Services.Calorie_Burned (Year       => Age,
-                                              Mass       => Weight,
-                                              Heart_Rate => Heart_Rate,
-                                              Minutes    =>  Minutes,
-                                              Gender     => Gender);
+ Calorie_Burned := Services.Calorie_Burned (Year       => Age,
+                                            Mass       => Weight,
+                                            Heart_Rate => Heart_Rate,
+                                            Minutes    =>  Minutes,
+                                            Gender     => Gender);
 
-   Put_Line ("calories burned : " & Calorie_Type'Image (Calorie_Burned));
+ Put_Line ("calories burned : " & Calorie_Type'Image (Calorie_Burned));
 
-   Body_Index := Services.Body_Mass_Index (Mass   => Weight,
-                                           Height => Height);
+ Body_Index := Services.Body_Mass_Index (Mass   => Weight,
+                                         Height => Height);
 
-   Put_Line ("BMI : " & BMI_Type'Image (Body_Index));
+ Put_Line ("BMI : " & BMI_Type'Image (Body_Index));
+
+ Daily_Calorie := Services.DTEE (Activity => Activity,
+                                 Mass     => Weight,
+                                 Height   => Height,
+                                 Year     => Age,
+                                 Gender   => Gender);
+
+ Put_Line ("Daily Calorie Required : " & Calorie_Type'Image (Daily_Calorie));
+
+
+ Ideal_Weight := Services.Ideal_Body_Weight (Height   => Height,
+                                             Gender => Gender);
+
+ Put_Line ("Ideal Weight: " & Mass_Type'Image (Ideal_Weight));
 
 exception
-   when E : others =>
-      Put_Line ("(FF)" & Ada.Exceptions.Exception_Information (E));
+ when E : others =>
+  Put_Line ("(FF)" & Ada.Exceptions.Exception_Information (E));
 end Health.Main;
 
--- utm/ic/sandbox/ehealth/sources/health-services.adb
+++ utm/ic/sandbox/ehealth/sources/health-services.adb
@@ -1,77 +1,169 @@
 package body Health.Services is
 
-   ------------------------
-   -- Calorie_Burned --
-   ------------------------
-   function  Calorie_Burned (Year        : Year_Type;
-                             Mass        : Mass_Type;
-                             Heart_Rate  : Heart_Rate_Type;
-                             Minutes     : Minute_Type;
-                             Gender      : Gender_Type) return Calorie_Type
-   is
-      Calorie_Burned : Calorie_Type := 0.0 * cal;
-   begin
-      case Gender is
-         when Male =>
-            Calorie_Burned :=
-              (
-               Year * 0.2017 * yr
-               - Mass * 0.09036 * mr
-               + Heart_Rate * 0.6309 * hr
-               - 55.0969
-              )
-                * Minutes * cal / (4.184 * minute);
-         when Female =>
-            Calorie_Burned :=
-              (
-               Year * 0.074 * yr
-               - Mass * 0.05741 * mr
-               + Heart_Rate * 0.4472 * hr
-               - 40.4022
-              )
-                * Minutes * cal / (4.184 * minute);
+ ------------------------
+ -- Calorie_Burned ------
+ ------------------------
+ function  Calorie_Burned (Year       : Year_Type;
+                          Mass       : Mass_Type;
+                          Heart_Rate : Heart_Rate_Type;
+                          Minutes    : Minute_Type;
+                          Gender     : Gender_Type) return Calorie_Type
+ is
+  Calorie_Burned : Calorie_Type := 0.0 * cal;
+ begin
+  case Gender is
+   when Male =>
+    Calorie_Burned :=
+     (
+      Year * 0.2017 * yr
+      - Mass * 0.09036 * mr
+      + Heart_Rate * 0.6309 * hr
+      - 55.0969
+     )
+      * Minutes * cal / (4.184 * minute);
 
-         when Other =>
+   when Female =>
+    Calorie_Burned :=
+     (
+      Year * 0.074 * yr
+      - Mass * 0.05741 * mr
+      + Heart_Rate * 0.4472 * hr
+      - 40.4022
+     )
+      * Minutes * cal / (4.184 * minute);
 
-            raise Program_Error with "not yet implemented";
-      end case;
-      return Calorie_Burned;
-   end Calorie_Burned;
+   when Other =>
+    raise Program_Error with "not yet implemented";
+  end case;
 
-   ------------------------
-   -- BMI-Body Mass Index--
-   ------------------------
+  return Calorie_Burned;
+ end Calorie_Burned;
+ ------------------------
+ -- BMI-Body Mass Index--
+ ------------------------
 
-   function Body_Mass_Index (Mass   : Mass_Type;
-                             Height : Height_Type) return BMI_Type
-   is
-      Body_Index :  BMI_Type := 0.0 * mi;
-   begin
-      Body_Index :=
-        (
-         Mass * mr
-         / Height**2
-         / lr
-        ) * mi ;
-      return Body_Index;
-   end;
-   ------------------------------
-   -- Basal Metabolice (BMR) --
-   ------------------------------
-     function  Basal_Metabolic_Rate (Mass   : Mass_Type;
-                                     Height : Height_Type;
-                                     Year   : Year_Type) return BMR_Type
-   is
-      Metabolic_Rate : BMR_Type := 0.0 * br;
-   begin
+ function Body_Mass_Index (Mass   : Mass_Type;
+                          Height : Height_Type) return BMI_Type
+ is
+  Body_Index : BMI_Type := 0.0 * mi;
+ begin
+  Body_Index :=
+   (
+    Mass * mr
+    / Height**2
+    / lr
+   ) * mi ;
+  return Body_Index;
+ end Body_Mass_Index;
+ ----------------
+ ------BMR -----
+ ----------------
 
-      return Metabolic_Rate;
-   end;
+ function   BMR (Mass   : Mass_Type;
+               Height : Height_Type;
+               Year   : Year_Type;
+               Gender : Gender_Type) return Calorie_Type
+ is
 
-   ------------------------------------------
-   -- Total Daily Energy Expenditure(TDEE) --
-   -------------------------------------------
+  Mebabolice_Rate : Calorie_Type := 0.0 * cal;
+ begin
+  case Gender is
+   when Male =>
+    Mebabolice_Rate := (
+                        Height * 6.25 * br
+                        + Mass * 9.99 * mr
+                        - Year * 4.92 * yr
+                        + 5.0
+                       ) * cal ;
 
-    --  TDEE = BMR * Activity
+   when Female =>
+    raise Program_Error with "not yet implemented";
+
+   when Other =>
+    raise Program_Error with "not yet implemented";
+  end case;
+
+  return Mebabolice_Rate;
+ end  BMR;
+ ------------------------------------------
+ -- Total Daily Energy Expenditure(TDEE) --
+ ------ TDEE = BMR * Activity-----------
+ -------------------------------------------
+
+ function   DTEE (Activity : Activity_Kind;
+                Mass     : Mass_Type;
+                Height   : Height_Type;
+                Year     : Year_Type;
+                Gender   : Gender_Type) return Calorie_Type
+ is
+  Daily_Calorie   : Calorie_Type := 0.0 * cal;
+  Mebabolice_Rate : Calorie_Type;
+ begin
+
+  case  Activity is
+  when Sedentary =>
+   Mebabolice_Rate:= BMR (Mass   => Mass,
+                          Height => Height,
+                          Year   => Year,
+                          Gender => Gender);
+   Daily_Calorie := Mebabolice_Rate * 1.2;
+
+  when Active =>
+    Mebabolice_Rate:= BMR (Mass   => Mass,
+                           Height => Height,
+                           Year   => Year,
+                           Gender => Gender);
+   Daily_Calorie := Mebabolice_Rate * 1.5;
+  end case;
+  return Daily_Calorie;
+ end  DTEE;
+ -----------------------------------
+ ------ Ideal Bocy Weight-----------
+ ------------------------------------
+
+ function   Ideal_Body_Weight (Height : Height_Type;
+                             Gender : Gender_Type) return Mass_Type
+ is
+  Ideal_Weight : Mass_Type := 0.0 * kg;
+ begin
+  case Gender is
+   when Male =>
+    Ideal_Weight := (
+                     Height * br - 100.0
+                     - Height * br - 100.0
+                    )*( 0.1 * kg);
+
+   when Female =>
+    raise Program_Error with "not yet implemented";
+    --  Women: Ideal Body Weight (kg) = [Height (cm) - 100] + ([Height (cm) - 100] x 15%)
+
+   when Other =>
+    raise Program_Error with "not yet implemented";
+
+  end case;
+  return Ideal_Weight;
+ end Ideal_Body_Weight;
+
+ -----------------------------------
+ ------ Calorie Deficiency ---------
+ -----------------------------------
+ pragma Compile_Time_Warning (True, "Calculate calorie deficient required to reach ideal weight");
+
+ function   Calorie_Defiency (Mass   : Mass_Type;
+                            Height : Height_Type;
+                            Gender : Gender_Type) return Calorie_Type
+ is
+  Ideal_weight  : Mass_Type;
+  Weight_Loss   : Mass_Type;
+ begin
+  --  1kg = 7700 cal
+  --  Weight_Loss = Current_weight - Ideal_weight
+  --  Convert Weight_Loss in calorie
+  --  calcuate calorie defficiency in 6month or 1year
+  return 0.0 * cal ;
+ end Calorie_Defiency;
+
+ pragma Compile_Time_Warning (True, "find weight in 1 year if consume more that TDEE");
+
 
 end Health.Services;
